437ff86e1b062757d40be68abdf3a2f349e025a6
Denis <lithiumdenis@gmail.com>
2017-10-14 22:05:49 +0300

    simplify with errors on process count 5,7,8,9, etc

--- Task 1. MPI taylor bessel/mpitey.c
+++ Task 1. MPI taylor bessel/mpitey.c
@@ -5,7 +5,10 @@
 #include <math.h>
 #include <cstdlib>
 
-#define MPI_ROOT_PROCESS 0
+#define MPI_ROOT_PROCESS 0	//id главного процесса
+#define A 0 				//начало интервала
+#define B 1					//конец интервала
+#define step 0.1			//шаг по х  0.1 0.00001; 
 
 double factorial(double f) {
 	if(f == 0)
@@ -13,16 +16,30 @@ double factorial(double f) {
 	return (f * factorial(f-1));
 }
 
-double GetApproxValue(double x, double Ac)
+double BesselJ0(double x)
 {
-	double sum = 0;
-	double k;
-
-    	for (k = 0; k < Ac; k++)
+	double k = 0;
+	double prevSum = 0;
+	double currSum = 0;
+	do
 	{
-		sum = sum + (pow(-1.0, k) / (pow(4.0, k) * pow(factorial(k), 2.0))) * pow(x, 2.0 * k);
+		prevSum = currSum;
+		currSum = currSum + (pow(-1.0, k) / (pow(4.0, k) * pow(factorial(k), 2.0))) * pow(x, 2.0 * k);
+		k++;
+	}while(fabs(currSum - prevSum) > 0);
+	return currSum;
+}
+
+void ResultToConsole(double *pointVector, double *approxValues, int N)
+{
+	//Выводим значения (точность - 10 знаков, выводить числа без отбрасывания нулей, показывать знак для положительных)
+	std::cout.precision(10);
+	std::cout.setf(std::ios::fixed | std::ios::showpos);
+	std::cout<<"x         y(x)\n";
+	for (int i = 0; i<N;i++)
+	{
+		std::cout<<pointVector[i]<< ' '<<approxValues[i]<<'\n';
 	}
-	return sum;
 }
 
 int main(int argc, char* argv[])
@@ -37,133 +54,134 @@ int main(int argc, char* argv[])
 	//Если процесс главный
 	if (rank == MPI_ROOT_PROCESS)
 	{
-		double A = 0; //начало интервала
-		double B = 1; //конец интервала
-		double Ac = 20; //число членов ряда Тейлора
-		double step = 0.00001; //шаг по х  0.1
-
-
-		int N = ((B-A) / step) + 1 + 0.5; //Количество точек (+0.5 решает проблему округления до int)
+		//Количество точек всего (+0.5 решает проблему округления до int)
+		int N = ((B-A) / step) + 1 + 0.5; 
 	
-		//Создаём массив точек, для которых будем считать функцию
+		//Массив со всеми точками от A до B
 		double *pointVector = new double[N];
-		//double step = (B-A)/(N-1);
+	
 		double cv = A;
 		for (int i=1;i<N;i++)
 		{
 			pointVector[i-1] = cv;
 			cv += step;
 		}
-		//Последняя точка может не совпадать с B, поэтому добавляем её отдельно
 		pointVector[N-1] = B;
 
-		//Теперь начинаем рассылку сообщений
-		//Отправляем эпсилон
-		MPI_Bcast(&Ac,1,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+		//Отправляем количество точек всего
+		MPI_Bcast(&N,1,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
 
-		double *approxValues = new double[N]; // приближённые значения
+		//Массив со всеми результатами вычислений
+		double *approxValues = new double[N]; 
 
-		int sendCount = N / procCount;
-		//Если количество процессов кратно числу точек, то всё хорошо
-		if (N % procCount == 0)
-		{ 
-			//Отправляем процессам сообщение с количеством точек, для которых необходимо рассчитать значение функции
-			MPI_Bcast(&sendCount,1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			//Отправляем сами точки
-			MPI_Scatter(pointVector,sendCount,MPI_DOUBLE,pointVector,sendCount,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			//Считаем функцию в своих точках
-			for (int i = 0; i<sendCount; i++)
-			{
-				approxValues[i] = GetApproxValue(pointVector[i],Ac);
-			}
+		//Посчитаем число точек для одного процесса
+		int sendCount = (N / procCount) + 0.5; //(+0.5 решает проблему округления до int)
 
-			//Теперь принимаем данные
-			MPI_Gather(approxValues,sendCount,MPI_DOUBLE,approxValues,sendCount,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-		}
-		//Иначе надо определить сколько кому отправлять
-		else
+		//std::cout << "N: " << N << std::endl;
+
+		//Вычислим, сколько элементов массива аргументов будет рассчитываться в каждом процессе
+		int *sendArray = new int[procCount];
+		for (int i = 0; i < procCount; i++)
 		{
-			//Отправляем сообщение другим процессам готовиться к векторному приёму/передаче
+			sendArray[i] = sendCount;
+			//Если нацело не делится, то прибавляем по единице к нескольким в начале
+			if(i < N % procCount)
 			{
-				int tmp = -1;
-				MPI_Bcast(&tmp,1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			}
-			//Формируем массив смещений и массив с числом элементов (смещения считаются в элементах от начала массива)
-			int *sendArray = new int[procCount];
-			int *offsetArray = new int[procCount];
-			for (int i = 0; i<procCount; i++)
-			{
-				sendArray[i] = sendCount;
-			}
-			for (int i = 0; i<N%procCount; i++)
 				sendArray[i]++;
-			offsetArray[0]=0;
-			for (int i=1; i<procCount; i++)
-			{
-				offsetArray[i]= sendArray[i-1]+offsetArray[i-1];
 			}
-			//Отправляем количество элементов для получения
-			MPI_Scatter(sendArray,1,MPI_INT,&sendArray[0],1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			//Отправляем данные
-			MPI_Scatterv(pointVector,sendArray,offsetArray,MPI_DOUBLE,pointVector,sendArray[0],MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			for (int i = 0;i<sendArray[0];i++)
-			{
-				approxValues[i] = GetApproxValue(pointVector[i],Ac);
-			}
-			//Теперь принимаем данные
-			MPI_Gatherv(approxValues,sendArray[0],MPI_DOUBLE,approxValues,sendArray,offsetArray,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+			//std::cout << "sendArray: " << sendArray[i] << std::endl;
 		}
-		//Выводим значения (точность - 10 знаков, выводить числа без отбрасывания нулей, показывать знак для положительных)
-		std::cout.precision(10);
-		std::cout.setf(std::ios::fixed | std::ios::showpos);
-		std::cout<<"x         y(x)\n";
-		for (int i = 0; i<N;i++)
-			std::cout<<pointVector[i]<< ' '<<approxValues[i]<<'\n';
+			
+		//Вычислим отступ от начала массива аргументов для каждого процесса
+		int *offsetArray = new int[procCount];
+		offsetArray[0] = 0; //для начального процесса отсчет sendArray[0] точек начинается с 0
+		//для остальных процессов отсчет sendArray[i] точек начинается со следующей после той, 
+		//на которой закончил отсчет предыдущий процесс
+		for (int i = 1; i < procCount; i++)
+		{
+			offsetArray[i] = sendArray[i-1] + offsetArray[i-1];
+			//std::cout << "offsetArray: " << offsetArray[i] << std::endl;
+		}
+
+		//Отправляем количество элементов каждому процессу
+		MPI_Scatter(sendArray,1,MPI_INT,&sendArray[0],1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+		//Отправляем отступ от начала каждому процессу
+		MPI_Scatter(offsetArray,1,MPI_INT,&offsetArray[0],1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+
+		/////////////////Тем временем ROOT PROCESS тоже считает свою часть/////////////////////////////
+
+		int pointCount = sendArray[0];
+		int offsetVal = offsetArray[0];
+
+		//Массив со всеми точками от offsetVal до offsetVal + pointCount
+		double *currPointVector = new double[pointCount];
+		
+		double cv2 = A + offsetVal * step;
+		//std::cout << rank << " rank, CV: " << cv << std::endl;
+		for (int i = 1; i < pointCount + 1; i++)
+		{	
+			currPointVector[i - 1] = cv2;
+			//std::cout << rank << " rank, pv: " << currPointVector[i - 1] << std::endl;
+			cv2 += step;
+		}
+
+		//Считаем функцию
+		for (int i = 0; i < pointCount; i++)
+		{
+			//std::cout << currPointVector[i] << std::endl;
+			approxValues[i + offsetVal] = BesselJ0(currPointVector[i + offsetVal]);
+		}
+		//////////////////////////////////////////////////////
+
+		//Теперь принимаем данные
+		MPI_Gather(approxValues,sendArray[0],MPI_DOUBLE,approxValues,sendArray[0],MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+		//Выводим всё в консоль	
+		//ResultToConsole(pointVector, approxValues, N);
 	}
+	//rank != ROOT_PROCESS
 	else
 	{
-		//Получаем эпсилон
-		double Ac;
-		MPI_Bcast(&Ac,1,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+		//Получаем общее количество точек
+		double N;
+		MPI_Bcast(&N,1,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
 		//Получаем количество точек
 		int pointCount;
-		MPI_Bcast(&pointCount,1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-		//Пришло положительное, значит число точек для всех процессов одинаковое и мы его получили
-		if (pointCount>0)
-		{
-			double *points = new double[pointCount];
-			//Получили точки
-			MPI_Scatter(0,0,MPI_DOUBLE,points,pointCount,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+		int offsetVal;
+		
+			//Получаем число точек
+			MPI_Scatter(0,0,MPI_DOUBLE,&pointCount,1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+
+			//std::cout << rank << "pcount " << pointCount << std::endl;
+			//Получаем значение отступа
+			MPI_Scatter(0,0,MPI_DOUBLE,&offsetVal,1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
+			//std::cout << rank << "offsetVal: " << offsetVal << std::endl;
+
+			//Массив со всеми точками от offsetVal до offsetVal + pointCount
+			double *currPointVector = new double[pointCount];
+		
+			double cv = A + offsetVal * step;
+			//std::cout << rank << " rank, CV: " << cv << std::endl;
+			for (int i = 1; i < pointCount + 1; i++)
+			{
+				
+				currPointVector[i - 1] = cv;
+				//std::cout << rank << " rank, pv: " << currPointVector[i - 1] << std::endl;
+				cv += step;
+				
+			}
+
 			//Считаем функцию
 			double *approxValues = new double[pointCount];
 			for (int i = 0; i<pointCount; i++)
 			{
-				approxValues[i] = GetApproxValue(points[i],Ac);
+				//std::cout << rank << " rank "<< currPointVector[i] << std::endl;
+				approxValues[i] = BesselJ0(currPointVector[i]);
+				std::cout << rank << " rank, approxValue:  "<< approxValues[i] << std::endl;
 			}
 			//Отправляем
 			MPI_Gather(approxValues,pointCount,MPI_DOUBLE,0,0,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-		}
-		else
-		//Если пришло отрицательное значение, значит число точек будет получено через scatter
-		{
-			//Получаем число точек
-			MPI_Scatter(0,0,MPI_DOUBLE,&pointCount,1,MPI_INT,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			//Получаем сами точки
-			double *points = new double[pointCount];
-			MPI_Scatterv(0,0,0,MPI_DOUBLE,points,pointCount,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-			//Считаем функцию
-			double *approxValues = new double[pointCount];
-			for (int i = 0; i<pointCount; i++)
-			{
-				approxValues[i] = GetApproxValue(points[i],Ac);
-			}
-			//Отправляем
-			MPI_Gatherv(approxValues,pointCount,MPI_DOUBLE,0,0,0,MPI_DOUBLE,MPI_ROOT_PROCESS,MPI_COMM_WORLD);
-		}
 	}
 	MPI_Finalize();
 	return 0;
 }
-
 //mpiCC mpitey.c -lm && mpiexec -n 11 ./a.out
-
