b226fbfec6aa58b6c7a3a86fbac595914f89f20e
Denis <lithiumdenis@gmail.com>
2017-11-05 17:45:37 +0300

    this f*****g works!
    really awesome

--- Task 3. Cuda fractal/Dragon curve/fractal.cu
+++ Task 3. Cuda fractal/Dragon curve/fractal.cu
@@ -5,6 +5,11 @@
 #include "stdbool.h"
 #include <cuda.h>
 #include "lodepng.h"
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
 
 #define iters 20
 #define PURPLE 0xFFCC00CC //В rgb фиолетовый 0xCC00CC, а в abgr это
@@ -14,24 +19,54 @@
 
 enum TURN
 {
-  LEFT,
-  RIGHT
+	LEFT,
+	RIGHT
 };
 
 enum DIRECTION
 {
-  D_LEFT,
-  D_UP,
-  D_RIGHT,
-  D_DOWN
+	D_LEFT,
+	D_UP,
+	D_RIGHT,
+  	D_DOWN
 };
 
+int *CopyArrayToGPU(int *HostArray, int NumElements)
+{
+    int bytes = sizeof(int) * NumElements;
+    int *DeviceArray;
+
+    // Allocate memory on the GPU for array
+    if (cudaMalloc(&DeviceArray, bytes) != cudaSuccess)
+    {
+        printf("CopyArrayToGPU(): Couldn't allocate mem for array on GPU.");
+        return NULL;
+    }
+
+    // Copy the contents of the host array to the GPU
+    if (cudaMemcpy(DeviceArray, HostArray, bytes, cudaMemcpyHostToDevice) != cudaSuccess)
+    {
+        printf("CopyArrayToGPU(): Couldn't copy host array to GPU.");
+        cudaFree(DeviceArray);
+        return NULL;
+    }
+
+    return DeviceArray;
+}
+
+/*
 //precondition: turnBuf is populated
-void createImage(int maxTurns, unsigned char* turnBuf, unsigned* pic, int x, int y, int iw)
+void createImage(int maxTurns, unsigned char* turnBuf, unsigned* pic, int x, int y, int iw, int ih)
 {
   int dir = D_UP;
+  
+  //fill image with BLACK (most pixels will be in final product)
+  for(int i = 0; i < iw * ih; i++)
+  {
+	  pic[i] = BLACK;
+  }
 
-  for(int i = 0; i <= maxTurns; i++)
+  for(int i = 0; i <= 30; i++)   //maxTurns
   {
     //move in current direction, writing pixels on segment to color
     pic[x + y * iw] = PURPLE;
@@ -70,14 +105,25 @@ void createImage(int maxTurns, unsigned char* turnBuf, unsigned* pic, int x, int
     else
     {
         pic[x + y * iw] = PURPLE;
-    }
+	}
+	
+
+
+
+	//cout << i << " iter,dir  is " << dir << endl;
   }
+
+
+  
+
+
   
 }
 
 
 
 
+*/
 
 
 
@@ -86,24 +132,208 @@ void createImage(int maxTurns, unsigned char* turnBuf, unsigned* pic, int x, int
 
 
 
-/*static const double Delta = 0.005491;
-static const double xMid = 0.745796;
-static const double yMid = 0.105089;
 
 
 static __global__
-void FractalKernel(const int width, unsigned char pic[])
+void FractalKernel(unsigned char *turnBuf, unsigned *pic, int maxTurns, int x, int y, int iw, int ih, int dir, int *d_dirs, int *d_x_arr, int *d_y_arr)
 {
+	/*for (int idx = blockIdx.x * blockDim.x + threadIdx.x; idx < maxTurns; idx += blockDim.x * gridDim.x) 
+	{
+		//move in current direction, writing pixels on segment to color
+
+		pic[x + y * iw] = PURPLE;
+		switch(dir)
+		{
+		  case D_LEFT:
+			pic[(x - 1) + y * iw] = PURPLE;
+			x -= (false ? 1 : 2);
+			break;
+		  case D_UP:
+			pic[x + (y - 1) * iw] = PURPLE;
+			y -= (false ? 1 : 2);
+			break;
+		  case D_RIGHT:
+			pic[(x + 1) + y * iw] = PURPLE;
+			x += (false ? 1 : 2);
+			break;
+		  case D_DOWN:
+			pic[x + (y + 1) * iw] = PURPLE;
+			y += (false ? 1 : 2);
+			break;
+		  default:;
+		}
+		//turn (except after last position update)
+		if(idx != maxTurns)
+		{
+		  if(turnBuf[idx] == LEFT)
+		  {
+			dir = (dir + 1) % 4;
+		  }
+		  else
+		  {
+			dir = (dir + 3) % 4;
+		  }
+		}
+		else
+		{
+			pic[x + y * iw] = PURPLE;
+		}
+		
+		__syncthreads();
+		
+		
+		
+		
+		
+		
+		
+		
+		//y[i] = a * x[i] + y[i];
+	}
+
+	*/
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     const int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    
+	
+	//printf("ih = %d", ih);
 
-    if (idx < width * width) { 
+    if (idx < maxTurns) { 
+
+		//const int col = idx % width;  //столбец
+        //const int row = (idx / width) % width; //строка
+
+		/*if(idx % 2 == 0)
+		{
+			pic[(d_x_arr[idx]) + d_y_arr[idx] * iw] = PURPLE;
+		}*/
+
+		
+			//pic[idx] = PURPLE;
+		
+
+		//int dir = D_UP;
+		
+		
+	  
+		//for(int i = 0; i <= maxTurns; i++)
+		//{
+		  //move in current direction, writing pixels on segment to color
+		  //pic[x + y * iw] = PURPLE;
+		  /*for(int idx = 0; idx < maxTurns; idx++)
+		  {
+			switch(d_dirs[idx])
+		  	{
+			case D_LEFT:
+			  pic[(d_x_arr[idx] - 1) + d_y_arr[idx] * iw] = PURPLE;
+			  //x -= (false ? 1 : 2);
+			  break;
+			case D_UP:
+			  //pic[x + (y - 1) * iw] = PURPLE;
+			  pic[(d_x_arr[idx]) + (d_y_arr[idx] - 1) * iw] = PURPLE;
+			  //y -= (false ? 1 : 2);
+			  break;
+			case D_RIGHT:
+			  //pic[(x + 1) + y * iw] = PURPLE;
+			  pic[(d_x_arr[idx] + 1) + d_y_arr[idx] * iw] = PURPLE;
+			  //x += (false ? 1 : 2);
+			  break;
+			case D_DOWN:
+			  pic[(d_x_arr[idx]) + (d_y_arr[idx] + 1) * iw] = PURPLE;
+			  //pic[x + (y + 1) * iw] = PURPLE;
+			  //y += (false ? 1 : 2);
+			  break;
+			default:;
+		  	}
+		  }*/
+
+		  //for (int idx = 0; idx < maxTurns; idx++)
+		  //{
+			  switch((int)d_dirs[idx])
+			  {
+					case D_LEFT:
+					  pic[((int)d_x_arr[idx] - 1) + (int)d_y_arr[idx] * iw] = PURPLE;
+					  //x -= (false ? 1 : 2);
+					  break;
+					case D_UP:
+					  //pic[x + (y - 1) * iw] = PURPLE;
+					  pic[((int)d_x_arr[idx]) + ((int)d_y_arr[idx] - 1) * iw] = PURPLE;
+					  //y -= (false ? 1 : 2);
+					  break;
+					case D_RIGHT:
+					  //pic[(x + 1) + y * iw] = PURPLE;
+					  pic[((int)d_x_arr[idx] + 1) + (int)d_y_arr[idx] * iw] = PURPLE;
+					  //x += (false ? 1 : 2);
+					  break;
+					case D_DOWN:
+					  pic[((int)d_x_arr[idx]) + ((int)d_y_arr[idx] + 1) * iw] = PURPLE;
+					  //pic[x + (y + 1) * iw] = PURPLE;
+					  //y += (false ? 1 : 2);
+					  break;
+					default:;
+			  }
+		  //}
+
+		  /*
+		  if((int)d_dirs[idx] == D_LEFT)
+		  {
+			pic[((int)d_x_arr[idx] - 1) + (int)d_y_arr[idx] * iw] = PURPLE;
+		  }
+		  else if ((int)d_dirs[idx] == D_UP)
+		  {
+			pic[((int)d_x_arr[idx]) + ((int)d_y_arr[idx] - 1) * iw] = PURPLE;
+		  }
+		  else if ((int)d_dirs[idx] == D_RIGHT)
+		  {
+			pic[((int)d_x_arr[idx] + 1) + (int)d_y_arr[idx] * iw] = PURPLE;
+		  }
+		  else if((int)d_dirs[idx] == D_DOWN)
+		  {
+			pic[((int)d_x_arr[idx]) + ((int)d_y_arr[idx] + 1) * iw] = PURPLE;
+		  }
+		  
+*/
+
+
+
+
+
+		  
+		  /*{
+			if(turnBuf[i] == LEFT)
+			{
+			  dir = (dir + 1) % 4;
+			}
+			else
+			{
+			  dir = (dir + 3) % 4;
+			}
+		  }
+		  else*/
+		  
+		//}
         
         
 
 
 
-        
+		
+		/*
 
         const int col = idx % width;  //столбец
         const int row = (idx / width) % width; //строка
@@ -130,14 +360,16 @@ void FractalKernel(const int width, unsigned char pic[])
                 } while ((depth > 0) && ((x2 + y2) < 5.0));
                 pic[row * width + col] = (unsigned char)depth;
             }
-        }
+		}
+		
+		*/
 
 
         
 
     }
 }
-*/
+
 
 int main(int argc, char *argv[])
 {
@@ -219,17 +451,287 @@ int main(int argc, char *argv[])
     //note: in real image, each position update is 2 pixels
     //also add one pixel border on all 4 edges
     int iw = (maxx - minx) * (false ? 1 : 2) + 3;
-    int ih = (maxy - miny) * (false ? 1 : 2) + 3;
+	int ih = (maxy - miny) * (false ? 1 : 2) + 3;
+	dir = D_UP;
 
 
-    unsigned* pic = (unsigned*)malloc(iw * ih * sizeof(unsigned));
-    //fill image with BLACK (most pixels will be in final product)
-    for(int i = 0; i < iw * ih; i++)
-      pic[i] = BLACK;
+	//create arrays of dots
+	int *h_dirs = new int[maxTurns];
+	int *x_arr = new int[maxTurns];
+	int *y_arr = new int[maxTurns];
+	//h_dirs[0] = D_UP;
+	//x_arr[0] = x;
+	//y_arr[0] = y;
+
+
+	//vector aaa = new vector[maxTurns];
+
+	dir = D_UP;
+	
+	for(int i = 0; i < maxTurns; i++)
+	{
+		y_arr[i] = y;
+		x_arr[i] = x; 
+		h_dirs[i] = dir; 
 
 
 
-    createImage(maxTurns, turnBuf, pic, x, y, iw);
+
+	  //move in current direction, writing pixels on segment to color
+	  //pic[x + y * iw] = PURPLE;
+	  switch(dir)
+	  {
+		case D_LEFT:
+		  //pic[(x - 1) + y * iw] = PURPLE;
+		  x -= (false ? 1 : 2);
+		  //x_arr[i + 1] = x; 
+		  break;
+		case D_UP:
+		  //pic[x + (y - 1) * iw] = PURPLE;
+		  y -= (false ? 1 : 2);
+		  //y_arr[i + 1] = y; 
+		  break;
+		case D_RIGHT:
+		  //pic[(x + 1) + y * iw] = PURPLE;
+		  x += (false ? 1 : 2);
+		  //x_arr[i + 1] = x; 
+		  break;
+		case D_DOWN:
+		  //pic[x + (y + 1) * iw] = PURPLE;
+		  y += (false ? 1 : 2);
+		  //y_arr[i + 1] = y; 
+		  break;
+		default:;
+	  }
+	  //turn (except after last position update)
+
+	  if(i != maxTurns)
+	  {
+		if(turnBuf[i] == LEFT)
+		{
+			dir = (dir + 1) % 4;
+			//h_dirs[i + 1] = dir;
+		}
+		else
+		{
+			dir = (dir + 3) % 4;
+			//h_dirs[i + 1] = dir;
+		}
+	  }
+	  else
+	  {
+		  //pic[x + y * iw] = PURPLE;
+	  }
+
+
+	  //cout << "x " << i << " is " << h_dirs[i] << endl;
+	}
+
+	/*cout << "maxTurns is " << maxTurns << endl;
+	for(int i = 0; i < maxTurns; i++)
+	{
+		cout << "x " << i << " is " << x << endl;
+	}*/
+
+	/////////////////////////////////////////
+
+	//cout << "dddvev" << endl;
+
+
+
+
+	int *d_dirs = CopyArrayToGPU(h_dirs, maxTurns);
+	int *d_x_arr = CopyArrayToGPU(x_arr, maxTurns);
+	int *d_y_arr = CopyArrayToGPU(y_arr, maxTurns);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
+	//allocate memory for d_dirs on device
+	unsigned *d_dirs = NULL;
+    if (cudaSuccess != cudaMalloc((void **)&d_dirs, maxTurns))
+    {
+        fprintf(stderr, "Failed to allocate device vector d_dirs\n");
+        exit(EXIT_FAILURE);
+	}
+	 
+	//Копирование h_dirs из хоста на девайс
+	cudaError_t err = cudaMemcpy(d_dirs, h_dirs, maxTurns, cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	{
+		 fprintf(stderr, "Failed to copy vector h_dirs from host to device (error code %s)!\n", cudaGetErrorString(err));
+		 exit(EXIT_FAILURE);
+	}
+	 
+	//allocate memory for d_x_arr on device
+	unsigned *d_x_arr = NULL;
+    if (cudaSuccess != cudaMalloc((void **)&d_x_arr, maxTurns))
+    {
+        fprintf(stderr, "Failed to allocate device vector d_x_arr\n");
+        exit(EXIT_FAILURE);
+	}
+	 
+	//Копирование x_arr из хоста на девайс
+	err = cudaMemcpy(d_x_arr, x_arr, maxTurns, cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	{
+		 fprintf(stderr, "Failed to copy vector x_arr from host to device (error code %s)!\n", cudaGetErrorString(err));
+		 exit(EXIT_FAILURE);
+	}
+	 
+	//allocate memory for d_y_arr on device
+	unsigned *d_y_arr = NULL;
+    if (cudaSuccess != cudaMalloc((void **)&d_y_arr, maxTurns))
+    {
+        fprintf(stderr, "Failed to allocate device vector d_y_arr\n");
+        exit(EXIT_FAILURE);
+	}
+	 
+	//Копирование y_arr из хоста на девайс
+	err = cudaMemcpy(d_y_arr, y_arr, maxTurns, cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	{
+		 fprintf(stderr, "Failed to copy vector y_arr from host to device (error code %s)!\n", cudaGetErrorString(err));
+		 exit(EXIT_FAILURE);
+ 	}
+
+*/
+
+	/////////////////////////////////////////
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//pic on host
+	unsigned* pic = (unsigned*)malloc(iw * ih * sizeof(unsigned));
+	
+	//fill image with BLACK (most pixels will be in final product)
+	for(int i = 0; i < iw * ih; i++)
+	{
+		pic[i] = BLACK;
+	}
+
+	//allocate memory for cuda pic on device
+	unsigned* pic_d;
+    if (cudaSuccess != cudaMalloc((void **)&pic_d, iw * ih * sizeof(unsigned))) 
+    {
+        fprintf(stderr, "could not allocate memory\n"); 
+        exit(-1);
+	}
+
+	//Копирование pic из хоста на девайс, т.е. из pic в pic_d
+	cudaError_t err = cudaMemcpy(pic_d, pic, iw * ih * sizeof(unsigned), cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	 {
+		 fprintf(stderr, "Failed to copy pic from host to device (error code %s)!\n", cudaGetErrorString(err));
+		 exit(EXIT_FAILURE);
+ 	}
+
+
+	//allocate memory for turnBuf on device
+	unsigned char *d_turnbuf = NULL;
+    if (cudaSuccess != cudaMalloc((void **)&d_turnbuf, maxTurns))
+    {
+        fprintf(stderr, "Failed to allocate device vector d_turnbuf\n");
+        exit(EXIT_FAILURE);
+	}
+	 
+	//Копирование turnBuf из хоста на девайс, т.е. из turnBuf в d_turnBuf
+	//printf("Copy input data from the host memory to the CUDA device\n");
+	err = cudaMemcpy(d_turnbuf, turnBuf, maxTurns, cudaMemcpyHostToDevice);
+	if (err != cudaSuccess)
+	 {
+		 fprintf(stderr, "Failed to copy vector turnBuf from host to device (error code %s)!\n", cudaGetErrorString(err));
+		 exit(EXIT_FAILURE);
+ 	}
+
+
+	
+	// compute
+    FractalKernel<<<(iw * ih + (ThreadsPerBlock - 1)) / ThreadsPerBlock, ThreadsPerBlock>>>(d_turnbuf, pic_d, maxTurns, x, y, iw, ih, dir, d_dirs, d_x_arr, d_y_arr);
+
+	//Return from cuda
+	if (cudaSuccess != cudaMemcpy(pic, pic_d, iw * ih * sizeof(unsigned), cudaMemcpyDeviceToHost)) 
+    {
+        fprintf(stderr, "copying from device failed\n"); 
+        exit(-1);
+	}
+	
+
+	/*
+	 //it WORKS!!!!!!!!!!!!!!
+	for (int idx = 0; idx < maxTurns; idx++)
+	{
+		switch((int)h_dirs[idx])
+		{
+	  		case D_LEFT:
+				pic[((int)x_arr[idx] - 1) + (int)y_arr[idx] * iw] = PURPLE;
+				//x -= (false ? 1 : 2);
+				break;
+	  		case D_UP:
+				//pic[x + (y - 1) * iw] = PURPLE;
+				pic[((int)x_arr[idx]) + ((int)y_arr[idx] - 1) * iw] = PURPLE;
+				//y -= (false ? 1 : 2);
+				break;
+	  		case D_RIGHT:
+				//pic[(x + 1) + y * iw] = PURPLE;
+				pic[((int)x_arr[idx] + 1) + (int)y_arr[idx] * iw] = PURPLE;
+				//x += (false ? 1 : 2);
+				break;
+	  		case D_DOWN:
+				pic[((int)x_arr[idx]) + ((int)y_arr[idx] + 1) * iw] = PURPLE;
+				//pic[x + (y + 1) * iw] = PURPLE;
+				//y += (false ? 1 : 2);
+				break;
+	  		default:;
+		}
+	}
+
+	
+	*/
+
+
+
+    
+
+
+
+    
+
+
+
+    //createImage(maxTurns, turnBuf, pic, x, y, iw, ih);
 
 
 
@@ -237,7 +739,8 @@ int main(int argc, char *argv[])
     char fname[64];
     sprintf(fname, "dragon.png");
     lodepng_encode32_file(fname, (unsigned char*) pic, iw, ih);
-    free(pic);
+	free(pic);
+	cudaFree(pic_d);
 
 
 
@@ -273,5 +776,5 @@ int main(int argc, char *argv[])
     return 0;*/
 }
 
-// nvcc fractal.cu -o cutey.out
-// ./cutey.out
+// make all
+// ./dragon
